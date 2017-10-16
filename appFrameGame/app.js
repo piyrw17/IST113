bfd6df4a5653271e66fb861e65fb13393cc36218
piyrw17 <anickhughes@gmail.com>
2017-10-16 15:48:21 -0400

    game

--- appFrameGame/app.js
+++ appFrameGame/app.js
@@ -3,7 +3,46 @@
 // using a function contructor form to create an object
 function MyApp()
 {
+	
 	var version = "v1.0";
+	var count = 0;
+	var userNumber;
+
+function new_game ()
+{
+	count = 0;
+  document.getElementById("userGuess").value = "";  
+ 
+}
+function Input()
+{
+	userNumber = parseInt(document.getElementById("userNum").value);
+}
+
+function make_guess()
+{  
+  var userGuess = parseInt(document.getElementById("userGuess").value);  
+  
+  
+  if ( userGuess > userNumber)
+  {
+	  count++;
+    document.getElementById("results").innerHTML = "The Guess was too high!";
+  }
+  else if ( userGuess < userNumber)
+  {
+	  count++;
+    document.getElementById("results").innerHTML = "The Guess was too low!";
+  }
+  else
+  {
+	  count++;
+    document.getElementById("results").innerHTML = "Congratulations the number was "+ userNumber;
+	document.getElementById("Count").innerHTML = "The number of guesses was "+ count;
+    new_game();
+  }
+}
+	
 
 	// creating a private function
 	function setStatus(message)
@@ -16,53 +55,17 @@ function MyApp()
 	{
 		$("#app>header").append(version);
 		setStatus("ready");
-	};
-	var count = 0;
+		
+		$("#userNumber").on("click", Input);
+		$("#guess").on("click", make_guess);
+	}
+	
+		
 
-function new_game ( )
-{
-	count = 0;
-  document.getElementById("userGuess").value = "";
-  //my_number = rand ( 100 );
-  
- 
-}
+	
+} 
 
-
-function make_guess ( )
-{
-	/*var x = document.getElementById("userNumber");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }*/
-  var userNumber = parseInt(document.getElementById("userNumber").value);
-  
-  var userGuess = parseInt(document.getElementById("userGuess").value);
-  
-  
-  
-  
-  if ( userGuess > userNumber )
-  {
-	  count++;
-    document.getElementById("results").innerHTML = "The Guess was too high!";
-  }
-  else if ( userGuess < userNumber )
-  {
-	  count++;
-    document.getElementById("results").innerHTML = "The Guess was too low!";
-  }
-  else
-  {
-	  count++;
-    document.getElementById("results").innerHTML = "Congratulations the number was "+ userNumber;
-	document.getElementById("Count").innerHTML = "The number of guesses was "+ count;
-    new_game ( );
-  }
-}
-} // end MyApp
+// end MyApp
 
 /* 	JQuery's shorthand for the document ready event handler
 		could be written: $(document).ready(handler);
@@ -75,3 +78,4 @@ $(function() {
 	window.app = new MyApp();
 	window.app.start();
 });
+
